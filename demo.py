99144062e74269ea147afd4ef91da3513454fb6f
TumeloKoko <u11046644@tuks.co.za>
2025-07-09 07:05:20 +0200

    Modify requirements to use GPU

--- demo.py
+++ demo.py
@@ -1,17 +1,14 @@
-from hate_classifier.configuration.s3_bucket_ops import S3Operations
+import tensorflow as tf
+import time
 
-s3 = S3Operations()
+with tf.device('/GPU:0'):
+    start = time.time()
+    a = tf.random.normal([10000, 10000])
+    b = tf.matmul(a, a)
+    print("GPU time:", time.time() - start)
 
-print(s3.list_objects())
-
-# files = s3.list_objects()
-
-# files[0].get("key")
-
-# s3.upload_file("requirements.in", "main/requirements.in")
-
-# s3.download_file(files[0].get("key"), "main/requirements.in")
-
-# s3.delete_file(files[0].get("key"))
-
-# print(s3.list_objects())
+with tf.device('/CPU:0'):
+    start = time.time()
+    a = tf.random.normal([10000, 10000])
+    b = tf.matmul(a, a)
+    print("CPU time:", time.time() - start)
